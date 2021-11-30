993575f20f9bb3fb2fe6151329d7da0ca07164b9
jbeneliezer <judahbeneliezer@gmail.com>
2021-11-30 13:57:06 -0500

    modified compiler

--- compiler/compile.py
+++ compiler/compile.py
@@ -40,18 +40,18 @@ with open("program2.txt", "r") as input:
         # li format
         # li imm, offset(rd)
         if lst[0] == "li":
-            op = "0" + format(int(lst[2]), "b").zfill(3) + format(int(lst[1])%0xFFFF, "b").zfill(16) + format(int(lst[3][1:]), "b").zfill(5)
+            op = "0" + format(int(lst[2])%0x8, "b").zfill(3) + format(int(lst[1])%0xFFFF, "b").zfill(16) + format(int(lst[3][1:])%0x1F, "b").zfill(5)
 
         # r4 format
         # op rd, rs1, rs2, rs3
         elif len(lst) == 5:
-            op = ops[lst[0]] + format(int(lst[4][1:]), "b").zfill(5) + format(int(lst[3][1:]), "b").zfill(5) + format(int(lst[2][1:]), "b").zfill(5) + \
-                format(int(lst[1][1:]), "b").zfill(5)
+            op = ops[lst[0]] + format(int(lst[4][1:])%0x1F, "b").zfill(5) + format(int(lst[3][1:])%0x1F, "b").zfill(5) + format(int(lst[2][1:])%0x1F, "b").zfill(5) + \
+                format(int(lst[1][1:])%0x1F, "b").zfill(5)
 
         # r3 format
         # op rd, rs1, rs2
         elif len(lst) == 4:
-            op = ops[lst[0]] + format(int(lst[3][1:]), "b").zfill(5) + format(int(lst[2][1:]), "b").zfill(5) + format(int(lst[1][1:]), "b").zfill(5)
+            op = ops[lst[0]] + format(int(lst[3][1:])%0x1F, "b").zfill(5) + format(int(lst[2][1:])%0x1F, "b").zfill(5) + format(int(lst[1][1:])%0x1F, "b").zfill(5)
 
         tests.append(op)
 
